74d8719992cc9c76258bda741c7a1f3dc1ea6516
Đông Trần <tptdong97@gmail.com>
2025-08-10 20:37:51 +0700

    update bundle id

--- app/api/.well-known/apple-app-site-association/route.ts
+++ app/api/.well-known/apple-app-site-association/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     const bundleId =
-        process.env.APPLE_BUNDLE_ID || "HGS93H2L2H.com.kpslholdings.ootnox.dev";
+        process.env.APPLE_BUNDLE_ID || "com.kpslholdings.ootnox.dev";
     const teamId = process.env.APPLE_TEAM_ID || "HGS93H2L2H";
     const appleAppSiteAssociation = {
         applinks: {
